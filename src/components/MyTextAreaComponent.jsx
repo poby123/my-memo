fe235fcca01d0e4b40d0bf3d314487e49738335e
poby123 <youngsm12@naver.com>
2021-06-13 23:30:14 +0900

    add textarea

---
+++ src/components/MyTextAreaComponent.jsx
@@ -0,0 +1,11 @@
+import './MyTextArea.scss';
+
+const MyTextAreaComponent = (props) => {
+  return (
+    <div className="MyTextAreaComponent">
+      <textarea className="textArea" />
+    </div>
+  );
+};
+
+export default MyTextAreaComponent;
---
+++ src/components/TextOptionsComponent.jsx
@@ -0,0 +1,12 @@
+const TextOptionsComponent = () =>{
+    return (
+        <div className="TextOptionsComponent">
+            <button>Option1</button>
+            <button>Option1</button>
+            <button>Option1</button>
+            <button>Option1</button>
+        </div>
+    )
+}
+
+export default TextOptionsComponent;
